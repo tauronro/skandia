cdc76cf71b38401b2d5d7e9ceba78be813508ccc
gustavo morales <brmorales@alianza.com.co>
2024-05-11 20:20:04 -0500

    GustavoMorales: se creo el modulo de la libreria angular material

--- src/app/shared/custom-material/custom-material.module.ts
+++ src/app/shared/custom-material/custom-material.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule
   ]
 })
 export class CustomMaterialModule { }
